137a6667dda4f313bc57ef4899b3e1892fcaad2f
trporter206 <39418076+trporter206@users.noreply.github.com>
2023-06-14 15:23:29 -0700

    more portland data

--- RecessTestGrounds/Models/Location.swift
+++ RecessTestGrounds/Models/Location.swift
@@ -9,16 +9,20 @@ import Foundation
 
 struct Location: Identifiable, Codable {
     var id: String
+    var name: String
     var sport: String
     var address: String
     var coordinates: [Double]
     var notes: String
+    var about: String?
     
-    init(sport: String, coordinates: [Double], notes: String, address: String) {
+    init(sport: String, name: String, coordinates: [Double], notes: String, address: String, about: String? = nil) {
         self.id = UUID().uuidString
         self.sport = sport
         self.coordinates = coordinates
         self.notes = notes
         self.address = address
+        self.name = name
+        self.about = about
     }
 }
